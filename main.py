420ad70038ba9e8c06e5e216501e01b23f8a7000
wolfsoad <pmramaia.engmec@gmail.com>
2022-07-26 19:46:40 +0100

    Add correction in SQLAlquemy Database URI

--- main.py
+++ main.py
@@ -20,7 +20,11 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+if os.environ.get('DATABASE_URL?sslmode=require').replace('postgres://', 'postgresql://'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL?sslmode=require').replace('postgres://', 'postgresql://')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
